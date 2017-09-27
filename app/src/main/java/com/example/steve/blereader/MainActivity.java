458c2f71fdb80c08fd1f11fc79fe5fb853f4b36d
steve <551619855@qq.com>
2017-09-27 19:34:43 +0800

    save to file ok.

--- app/src/main/java/com/example/steve/blereader/MainActivity.java
+++ app/src/main/java/com/example/steve/blereader/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.steve.blereader;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Set;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity
@@ -26,6 +36,78 @@ public class MainActivity extends AppCompatActivity
 
     Boolean is_searching = false; //Flag for collecting data(IMU and BLE)
     Boolean keep_true = true; // aux value.
+    float[] sensorvalues = new float[10];// time acc_x acc_y acc_z gyr_x gyr_y gyr_z mag_x mag_y mag_z
+    long save_acc_time = 0;
+    boolean is_acc_updated = false;
+    boolean is_gyr_updated = false;
+    boolean is_mag_updated = false;
+
+    //    File ble_file = null;
+//    File sensor_file = null;
+    FileOutputStream ble_file ;//new FileOutputStream();
+    FileOutputStream sensor_file ;//= new FileOutputStream();
+
+
+    private class SensorDataSaver implements Runnable {
+        private final static String TAG = "SensorDataSaver";
+
+        int writed_counter = 0;
+        StringBuilder local_sb = new StringBuilder();
+
+        @Override
+        public void run() {
+            try {
+                while (keep_true) {
+                    if (is_searching) {
+                        if (is_acc_updated && is_gyr_updated && is_mag_updated) {
+                            // TODO: save to file
+                            try {
+                                Log.i(TAG,local_sb.toString());
+                                if (writed_counter > 10) {
+                                    sensor_file.write(local_sb.toString().getBytes(),0,local_sb.toString().getBytes().length);
+                                    sensor_file.flush();
+                                    local_sb.delete(0,local_sb.length());
+                                    writed_counter = 0;
+                                } else {
+                                    writed_counter +=1;
+                                    local_sb.append(String.valueOf(save_acc_time)+",");
+                                    for(int i=1;i<9;++i)
+                                    {
+
+                                       local_sb.append(String.format("%.20f,",sensorvalues[i]));
+                                    }
+                                    local_sb.append(String.valueOf(sensorvalues[9])+"\n");
+
+                                }
+                                is_acc_updated = false;
+                                is_gyr_updated = false;
+                                is_mag_updated = false;
+
+                            }catch (Exception e){
+                                Log.i(TAG,"error in write to file (sensor file)");
+                                System.out.print("error while save file");
+                            }
+
+
+
+
+                        } else {
+                            // 100 ns...
+                            sleep(0, 100);
+                        }
+
+                    } else {
+                        // sleep for 100 ms
+                        sleep(100);
+                    }
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
 
     /**
      * For Maintain the collecting state.
@@ -44,16 +126,22 @@ public class MainActivity extends AppCompatActivity
 
             try {
                 while (keep_true) {
-                        if (is_searching) {
-                            if (mBluetoothAdapter.isDiscovering()) {
-                                Log.d(TAG,"wait for current discovering");
-                                sleep(5);
-                            } else {
-                                mBluetoothAdapter.startDiscovery();
-                            }
+                    if (is_searching) {
+                        if (mBluetoothAdapter.isDiscovering()) {
+                            Log.d(TAG, "wait for current discovering");
+                            sleep(5);
                         } else {
-                            sleep(100);
+                            if (ble_file != null) {
+                                ble_file.flush();
+                            }
+                            mBluetoothAdapter.startDiscovery();
                         }
+                    } else {
+                        if (mBluetoothAdapter.isDiscovering()) {
+                            mBluetoothAdapter.cancelDiscovery();
+                        }
+                        sleep(100);
+                    }
                 }
 
             } catch (Exception e) {
@@ -87,21 +175,30 @@ public class MainActivity extends AppCompatActivity
             if (action.equals(BluetoothDevice.ACTION_FOUND)) {
                 BluetoothDevice device = intent
                         .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                // 显示在TextView上
-                mTextView.append(String.valueOf(System.currentTimeMillis())+","+
-                        device.getAddress() +","+
-                                Integer.toString(rssi)+ "\n");
-//                }
-                // 搜索完成
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+//                mTextView.append(String.valueOf(System.currentTimeMillis()) + "," +
+//                        device.getAddress() + "," +
+//                        Integer.toString(rssi) + "\n");
+
+                // write to file at each moments but flush when discovering ended(see : BLERunnable).
+                StringBuilder tmp_sb = new StringBuilder();
+                tmp_sb.append(String.valueOf(System.currentTimeMillis()) + "," +
+                        device.getAddress() + "," +
+                        Integer.toString(rssi) + "\n");
+                try {
+
+                    ble_file.write(tmp_sb.toString().getBytes(),0,tmp_sb.toString().getBytes().length);
+//                    ble_file.write("aaa".getBytes());
+                } catch (Exception e) {
+                    mTextView.append("broadcastReceiver" + e.getLocalizedMessage()+e.toString() + "\n");
+
+                }
             } else if (action
                     .equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
                 setProgressBarIndeterminateVisibility(false);
-//                setTitle("搜索蓝牙设备");
             }
         }
     };
-
 
 
     @Override
@@ -113,6 +210,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.showText);
+        mTextView.setGravity(Gravity.BOTTOM);// keep the point of view at last line.
+
+
         mControlButton = (Button) findViewById(R.id.ControlButton);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -130,24 +230,78 @@ public class MainActivity extends AppCompatActivity
         ble_discovering_thread = new Thread(bleRunnable);
         ble_discovering_thread.start();
 
+        SensorDataSaver saver = new SensorDataSaver();
+        new Thread(saver).start();
+
+
         // Sensor manager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //// Getting permission...
+        String[] needed_permission = new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS
+        };
+
+        boolean permission_ok = true;
+
+        for(String permission: needed_permission)
+        {
+            if(ContextCompat.checkSelfPermission(this,
+                    permission)!= PackageManager.PERMISSION_GRANTED)
+            {
+                permission_ok = false;
+                mTextView.append(String.valueOf(permission_ok)+"\n");
+            }
+        }
+
+        if(!permission_ok)
+        {
+            ActivityCompat.requestPermissions(
+                    this,
+                    needed_permission,
+                    1
+            );
+        }
+
 
 
     }
 
 
-
-
-
     public void onClick_Search(View v) {
 //        setProgressBarIndeterminateVisibility(true);
         if (is_searching) {
+
+            /**
+             * STOP
+             */
             is_searching = false;
 //            setTitle("Searching stopped");
             mControlButton.setText("Start");
 
             mSensorManager.unregisterListener(this);
+
+            // TODO: close file
+            try {
+
+                if (ble_file != null) {
+                    ble_file.close();
+                }
+                if (sensor_file != null) {
+                    sensor_file.close();
+                }
+
+                ble_file = null;
+                sensor_file = null;
+            } catch (IOException e) {
+                Log.d("test", e.toString());
+                mTextView.append(e.getLocalizedMessage()+e.toString() + "\n");
+            }
+
+
+
 
         } else {
             is_searching = true;
@@ -169,6 +323,41 @@ public class MainActivity extends AppCompatActivity
                     mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
                     SensorManager.SENSOR_DELAY_FASTEST);
 
+
+            // TODO: openfile
+            long time_now = System.currentTimeMillis();
+            try {
+
+
+
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    File sdCardDir = Environment.getExternalStorageDirectory();
+
+                    File ble_file_creator = new File(sdCardDir.getCanonicalPath() + "/a/"+ String.valueOf(time_now) + "ble.txt");
+                    boolean is_ble_file_ok =  ble_file_creator.createNewFile();
+                    mTextView.append("is ble file ok :" + String.valueOf(is_ble_file_ok) + "\n");
+
+
+                    File sensor_file_creator = new File( sdCardDir.getCanonicalPath() + "/a/"+ String.valueOf(time_now) + "sensor.txt");
+
+                    boolean is_seasor_file_ok = sensor_file_creator.createNewFile();
+                    mTextView.append("is sensor file ok : " + String.valueOf(is_seasor_file_ok)+"\n");
+
+                    ble_file = new FileOutputStream(ble_file_creator.getCanonicalPath());
+                    sensor_file = new FileOutputStream(sensor_file_creator.getCanonicalPath());
+
+                    assert(ble_file!=null && sensor_file!=null);
+                } else {
+                    mTextView.append("Cannot access SDCARD!!!\n");
+                }
+
+            } catch (IOException e) {;//
+                Log.d("test", e.toString());
+                e.printStackTrace();
+                mTextView.append(e.getLocalizedMessage()+e.toString() + "\n");
+            }
+
+
         }
 
     }
@@ -180,22 +369,65 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    /**
+     * backcall for sensor information
+     *
+     * @param sensorEvent
+     */
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
         float[] values = sensorEvent.values;
+        StringBuilder sb = null;//= new StringBuilder();
+
 
         int sensorType = sensorEvent.sensor.getType();
-        switch (sensorType)
-        {
+        switch (sensorType) {
             case Sensor.TYPE_ACCELEROMETER:
+//                sb = new StringBuilder();
+//                sb.append(String.valueOf(System.currentTimeMillis()));
+//                sb.append(":acc:");
+//                sb.append(String.valueOf(values[0]) + ',');
+//                sb.append(String.valueOf(values[1]) + ',');
+//                sb.append(String.valueOf(values[2]) + '\n');
+//                mTextView.append(sb.toString());
+//                sensorvalues[0] = System.currentTimeMillis();
+                save_acc_time = System.currentTimeMillis();
+                sensorvalues[1] = values[0];
+                sensorvalues[2] = values[1];
+                sensorvalues[3] = values[2];
+                is_acc_updated = true;
+
 
                 break;
             case Sensor.TYPE_GYROSCOPE:
+//                sb = new StringBuilder();
+//                sb.append(String.valueOf(System.currentTimeMillis()));
+//                sb.append(":gyr:");
+//                sb.append(String.valueOf(values[0]) + ',');
+//                sb.append(String.valueOf(values[1]) + ',');
+//                sb.append(String.valueOf(values[2]) + '\n');
+//                mTextView.append(sb.toString());
+                sensorvalues[4] = values[0];
+                sensorvalues[5] = values[1];
+                sensorvalues[6] = values[2];
+                is_gyr_updated = true;
 
                 break;
             case Sensor.TYPE_MAGNETIC_FIELD:
+//                sb = new StringBuilder();
+//                sb.append(String.valueOf(System.currentTimeMillis()));
+//                sb.append(":mag:");
+//                sb.append(String.valueOf(values[0]) + ',');
+//                sb.append(String.valueOf(values[1]) + ',');
+//                sb.append(String.valueOf(values[2]) + '\n');
+//                mTextView.append(sb.toString());
 
+                sensorvalues[7] = values[0];
+                sensorvalues[8] = values[1];
+                sensorvalues[9] = values[2];
+
+                is_mag_updated = true;
                 break;
 
         }
